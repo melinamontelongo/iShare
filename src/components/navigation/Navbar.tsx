8c07c160c534f362110498603e8d06db8f704e9f
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-08-14 21:04:20 -0300

    feat: logo for small screens

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -11,17 +11,18 @@ export default async function Navbar() {
         <header className="bg-zinc-50 dark:bg-zinc-950 h-fit shadow-sm fixed top-0 left-0 right-0 z-50 px-4 sm:px-6 lg:px-8 py-2">
             <div className="flex flex-row justify-between items-center">
                 <div className="flex items-center gap-2">
-                    <Link className="block text-teal-500 dark:text-teal-600" href="/">
+                    <Link className="text-teal-500 dark:text-teal-600 sm:block hidden md:px-5 md:py-2.5 px-3 py-2" href="/">
                         <span className="sr-only">Home</span>
                         <Image src="/ishare_logo.png" alt="ishare logo" height={100} width={100} />
-                        {/* <h2 className="md:text-4xl text-xl font-bold sm:block hidden">iShare</h2>
-                        <h2 className="sm:hidden text-4xl font-bold">iS</h2> */}
                     </Link>
-
+                    <Link className="block text-teal-500 dark:text-teal-600 sm:hidden block" href="/">
+                        <span className="sr-only">Home</span>
+                        <Image src="/ishare_logo_sm.png" alt="ishare logo" height={50} width={50} />
+                    </Link>
                     <Searchbar />
                 </div>
                 <div className="flex items-center gap-2">
-                    <div className="block rounded-md md:px-5 md:py-2.5 px-3 py-2 text-sm font-medium transition">
+                    <div className="block rounded-md text-sm font-medium transition">
                         <ThemeChanger />
                     </div>
                     <div className="sm:flex">
@@ -31,7 +32,7 @@ export default async function Navbar() {
                             :
                             (
                                 <Link
-                                    className="block rounded-md bg-teal-500 dark:bg-teal-600 md:px-5 md:py-2.5 px-3 py-2 text-sm font-medium text-white transition hover:bg-teal-700"
+                                    className="block rounded-md bg-teal-500 dark:bg-teal-600 md:px-5 md:py-2.5 p-1 md:text-sm text-xs font-medium text-white transition hover:bg-teal-700"
                                     href="/sign-in">
                                     Sign In
                                 </Link>
